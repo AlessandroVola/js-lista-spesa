69c9e3d203419db915e8883a3db5c2bc466fd37f
AlessandroVola <volaalessandro@gmail.com>
2022-08-30 21:08:48 +0200

    ciclo while in console.log

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// creo array per lista della spesa
+const groceryList = [`Mele`, `Pere`, `Limoni`, `Latte`, `Biscotti`]
+
+// inizializzo la variabile del contatore
+i = 0
+
+// ciclo while
+while (i < groceryList.length) {
+    console.log(groceryList[i])
+    i++;
+}
