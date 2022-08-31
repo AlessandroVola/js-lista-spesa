3ef81819a656501a42bfb4ca5a91642a4a0acc29
AlessandroVola <volaalessandro@gmail.com>
2022-08-31 10:12:53 +0200

    correzioni2

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const groceryList = [`Mele`, `Pere`, `Limoni`, `Latte`, `Biscotti`];
 // inizializzo la variabile del contatore
 let i = 0;
 
-const htmlContainer = document.querySelector("container");
+const htmlContainer = document.querySelector(".container");
 htmlContainer.innerHTML = ``
 
 // ciclo while
